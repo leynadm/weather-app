67d93919b93004bc6d4c9e44af405072ef8a93a1
Leynad <matei_daniel@outlook.com>
2022-12-06 11:49:18 +0100

    Try and fix the http request to the API

--- src/fetch-weather.js
+++ src/fetch-weather.js
@@ -1,9 +1,10 @@
 async function fetchWeatherDataJSON(city) {
   try {
     const response = await fetch(
-        `http://api.openweathermap.org/data/2.5/weather?q=${  city  }&APPID=130589d71f758c19a3a8b6d6e7719a49&units=metric`,
+        `https://api.openweathermap.org/data/2.5/weather?q=${  city  }&APPID=130589d71f758c19a3a8b6d6e7719a49&units=metric`,
       { mode: "cors" }
     );
+    
     const weatherData = await response.json();
     const myWeatherDetails = {
       weatherCityName: weatherData.name,
